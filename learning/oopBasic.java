1bf9a4d29bb14fc0446794242b9e88625c6eee8b
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2026-02-05 02:00:19 +0600

    add initial implementation of Pen and Student classes with constructors and methods

---
+++ learning/oopBasic.java
@@ -0,0 +1,38 @@
+package learning;
+
+class Pen{
+    String color;
+    String type;
+
+    public void printColor() {
+        System.out.println(this.color);
+    }
+    
+}
+
+class Student{
+    String name;
+    int age;
+    
+    public void printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+    Student(Student s2){
+        this.name = s2.name;
+        this.age = s2.age;
+    }
+    Student(){
+
+    }
+}
+
+public class oopBasic {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Sakib";
+        s1.age = 23;
+        Student s2 = new Student(s1);
+        s2.printInfo();
+    }
+}
